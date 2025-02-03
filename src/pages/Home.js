6a64a0c6f83b09980e87a032b67d676738602bb5
magdiperper12 <162820515+magdiperper12@users.noreply.github.com>
2025-02-03 14:24:54 +0200

    adding type animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,17 +11,18 @@ import {
 import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { BiArrowToRight } from 'react-icons/bi';
 import { Link } from 'react-router-dom';
+import { TypeAnimation } from 'react-type-animation';
 
 function Home() {
 	const { darkMode } = useDarkMode(); // Use the darkMode state from context
 
 	return (
 		<div
-			className={`relative ${
+			className={`relative -mt-16 md:-mt-0  ${
 				darkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'
 			}`}>
 			<div
-				className={`absolute cursor-pointer -start-6 top-1/4 flex flex-col gap-4 py-3 -mt-20 px-2 rounded-full shadow-lg ${
+				className={`absolute cursor-pointer -start-6 top-1/4 flex flex-col gap-4 py-3 -mt-20 px-2 rounded-full shadow-xl ${
 					darkMode ? 'bg-gray-700 text-blue-300' : 'bg-white text-blue-600'
 				}`}>
 				<a
@@ -61,17 +62,27 @@ function Home() {
 				</Link>
 			</div>
 			<section
-				className={`container flex lg:w-3/4 justify-center items-center py-16 px-3 ${
+				className={`container md:mt-10 xl:mt-auto md:mx-4 xl:mx-auto flex xl:w-3/4 justify-center items-center py-16 px-3 ${
 					darkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'
 				} mx-auto`}>
-				<div className='relative pt-36 lg:pt-0'>
+				<div className='relative pt-36 xl:pt-0'>
 					<h1
-						className={`text-white p-7 bg-gray-700 rounded-xl   lg:ps-80 text-2xl sm:text-3xl md:text-5xl lg:text-5xl font-bold mb-5`}>
-						Hi, I'm Magdi. I'm a Front-End Developer focused on making a high-
-						performance web applications.
+						className={`text-white p-7 md:pt-10 xl:pt-auto bg-gray-700 rounded-xl    xl:ps-80  sm:text-3xl md:text-3xl xl:text-5xl font-bold mb-5`}>
+						<TypeAnimation
+							sequence={[
+								1000,
+								"Hi, I'm Magdi. I'm a Front-End Developer focused on making a high-performance web applications.",
+								1000,
+								'How can I help you ....?',
+								1000,
+							]}
+							wrapper='span'
+							speed={50}
+							repeat={Infinity}
+						/>
 					</h1>
 					<p
-						className={`mb-5 px-7 text-base sm:text-lg md:text-xl lg:text-2xl  lg:ps-80 font-medium ${
+						className={`mb-5 px-7 text-base sm:text-xl md:text-xl xl:text-2xl  xl:ps-80 font-medium ${
 							darkMode ? 'text-gray-200' : 'text-black'
 						}`}>
 						I learned to make websites on my own. I know how fun and rewarding
@@ -89,7 +100,7 @@ function Home() {
 							</a>
 						</div>
 					</p>
-					<div className='absolute start-1/6 md:start-1/4 lg:start-0 shadow-lg shadow-gray-700 -top-0  md:-top-24 lg:top-16 rounded-full overflow-hidden w-44 h-44  md:w-72 md:h-72 lg:w-80 lg:h-80'>
+					<div className='absolute  start-1/6 md:start-1/4 lg:start-1/3 xl:start-0 shadow-xl shadow-gray-700 -top-1  md:-top-28 md:z-0 xl:top-16 rounded-full overflow-hidden w-40 h-40  md:w-72 md:h-72 xl:w-80 xl:h-80'>
 						<img
 							src={image}
 							className='mx-auto rounded-full'
@@ -100,7 +111,7 @@ function Home() {
 			</section>
 
 			<section
-				className={`container flex lg:flex-row md:flex-col flex-col  w-full justify-between items-center py-16 px-10 ${
+				className={`container mx-4 flex xl:flex-row md:flex-col flex-col  w-full justify-between items-center py-16 px-10 ${
 					darkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'
 				}`}>
 				<div>
@@ -113,7 +124,7 @@ function Home() {
 					<h1 className='text-3xl sm:text-4xl md:text-5xl font-semibold mb-6 animate-bounce'>
 						Frontend Developer
 					</h1>
-					<p className='max-w-4xl text-lg sm:text-xl md:text-2xl'>
+					<p className='max-w-4xl text-xl sm:text-xl md:text-2xl mb-5'>
 						I am a frontend web developer with more than two years of
 						experience. Proven experience building successful products for
 						clients across several countries.
@@ -122,7 +133,7 @@ function Home() {
 				<div>
 					<img
 						src={image1}
-						className='h-auto w-full rounded-xl mx-auto  lg:w-2/3'
+						className='h-auto w-full rounded-xl mx-auto  xl:w-2/3'
 						alt='logo'
 					/>
 				</div>
